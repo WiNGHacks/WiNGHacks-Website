f77f27cdbe2a5993bb5b8f2f8812d282df31b221
mariapasaylo <mnapasaylo@gmail.com>
2024-01-23 08:14:44 -0500

    Started landing page hero
    Added place holder logo and links to apply now and become a sponsor. Centered all elements

--- front-end/src/components/pages/Home.js
+++ front-end/src/components/pages/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Home.css';
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='Home'>
+      <section className='Home-hero'>
+        <h1>Welcome to WiNGHacks!</h1>
+        <img src='./public/logo192.png' alt='WiNGHacks Logo'></img>
+        {/* PlaceHolder href for now ask Christine/Annie for links */}
+        <br></br>
+        <a href="index.html">Apply now!</a>
+        <br></br>
+        <a href="index.html">Become a Sponsor</a>
+      </section>
+      <section className='Home-About'>
+        
+      </section>
+    </div>
   )
 }
 
