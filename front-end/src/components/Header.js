b6a5edf6f359a9e00ed63371d132e44e3542b30c
AnanyaKakaveti <a.kakaveti@ufl.edu>
2024-01-24 15:01:51 -0500

    navbar with separate pages

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -5,22 +5,6 @@ import logo from './pictures/littleLady.png'
 
 const Header = () => {
   return (
-    // <div className = "navbar">
-
-    //   <Link to="/" className = "logo-container">
-    //     <img src={logo} alt="Logo"/>
-    //     <b className='image-text'>WiNGHacks</b>
-    //   </Link>
-
-    //   <div className = "links">
-    //     <Link to="/aboutus" className = "link">About Us</Link>
-    //     <Link to="/sponsors" className = "link">Sponsors</Link>
-    //     <Link to="/faq" className = "link">FAQ</Link>
-
-    //     <Link to="/faq" className = "link">Apply Now</Link> 
-    //   </div>
-    // </div> 
-
     <div className = "navbar">
 
       <Link to="/" className = "logo-container">
@@ -29,16 +13,31 @@ const Header = () => {
       </Link>
 
       <div className = "links">
-        <div className = "link">About Us</div>
-        <div className = "link">Sponsors</div>
-        <div className = "link">FAQ</div>
+        <Link to="/aboutus" className = "link">About Us</Link>
+        <Link to="/sponsors" className = "link">Sponsors</Link>
+        <Link to="/faq" className = "link">FAQ</Link>
 
-        {/* PlaceHolder for now Update Later */}
-        <div className = "link" >Apply Now</div> 
+        <Link to="/faq" className = "link">Apply Now</Link> 
       </div>
-
-
     </div> 
+
+    // <div className = "navbar">
+
+    //   <Link to="/" className = "logo-container">
+    //     <img src={logo} alt="Logo"/>
+    //     <b className='image-text'>WiNGHacks</b>
+    //   </Link>
+
+    //   <div className = "links">
+    //     <div className = "link">About Us</div>
+    //     <div className = "link">Sponsors</div>
+    //     <div className = "link">FAQ</div>
+
+    //     {/* PlaceHolder for now Update Later */}
+    //     <div className = "link" >Apply Now</div> 
+    //   </div>
+
+    // </div> 
     
   )
 }
