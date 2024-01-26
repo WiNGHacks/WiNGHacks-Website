21065b9df9cc0dd85fc29508f27b892bc30e6dc5
christinelin <christinelin.7771@gmail.com>
2024-01-26 00:50:09 -0500

    Added drop down pdf in sponsor page

--- front-end/src/components/pages/Sponsors.js
+++ front-end/src/components/pages/Sponsors.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Sponsors = () => {
+
+  const [openPDF, setOpenPDF] = useState(false)
+
   const onButtonClick = () => {
-    const pdfUrl = "SponsorshipPacket.pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.download = "document.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    // const pdfUrl = "SponsorshipPacket.pdf";
+    // const link = document.createElement("a");
+    // link.href = pdfUrl;
+    // link.download = "document.pdf";
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+
+    setOpenPDF(!openPDF)
+
+
+    
   };
 
   return (
     <div>
       <h1>Sponsors</h1>
       <button onClick ={onButtonClick}>Become a Sponsor</button>
+
+      {openPDF? (<div><iframe src="SponsorshipPacket.pdf" width="100%" height="500px" /></div>):(<div></div>)}
+      
       </div>
   )
 }
