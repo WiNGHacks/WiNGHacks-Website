ff7e59919e3f2e803de9567efe022aaf972c715f
mariapasaylo <mnapasaylo@gmail.com>
2024-02-05 21:30:24 -0500

    Added templates for members
    Created a template for member images. Attempted to make a filter system for each committee.

--- front-end/src/components/pages/AboutUs.js
+++ front-end/src/components/pages/AboutUs.js
@@ -6,17 +6,33 @@ import csk from '../pictures/org_logos/csk_logo.png'
 import gwc from '../pictures/org_logos/gwc_logo.jpeg'
 import wece from '../pictures/org_logos/wece_logo.jpeg'
 import wicys from '../pictures/org_logos/wicys_logo.jpeg'
+import CommitteeMemberItem from './CommitteeMemberItem.js'
+import { MemberList } from '../../data/MemberList'
+import $ from 'jquery'
 
-import headshots from '../pictures/littleLady.PNG'
-import headshots2 from '../pictures/Frogger.PNG'
-
-
-const text = `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Enim sed faucibus turpis in eu mi. Porttitor lacus luctus accumsan tortor posuere.
-`;
-const committeeNames =["Main Organizers", "Awards", "Budget", "PR", "Merch", "Operations", "Sponsorship", "Tech-Support", "Website", "Workshop"];
 
 const AboutUs = () => {
+  const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Enim sed faucibus turpis in eu mi. Porttitor lacus luctus accumsan tortor posuere.`;
+  const committeeNames =["Organizers", "Awards", "PR", "Merch", "Operations", "Sponsorship", "Tech-Support", "Website", "Workshop"]; 
+
+  $(() => {
+    $(".btn-committee-names").find("a").on(function (e){
+      {/*retrieves value from data-filter */}
+      var theFilter = $(this).data('filter');
+      e.preventDefault();
+      {/*MARIA ATTEMPT TO MAKE BUTTON HOVER COLOR TO STICK*/}
+      $(".btn-committee-names").find("a").removeClass("active");
+      $(this).addClass("active");
+
+      {/*Only show members in a specific committee*/}
+      $(".members").find("li").show();
+      $(".members").find("li").not(theFilter).hide();
+
+
+    });
+  });
+
+
   return (
     <div>
       <div className='About'>
@@ -31,7 +47,6 @@ const AboutUs = () => {
           </div>
           <div className='who-we-are-child-container'>
             <div className='collaborators-container'>
-              {/*MARIA-How to use variable to store the logos so don't have to repeat img every time?*/}
                 <img  src={wicse} className='logo-who-we-are' alt='WiNGHacks WiCSE Logo'></img>
                 <img  src={csk} className='logo-who-we-are' alt='WiNGHacks CSK Logo'></img>
                 <img  src={gwc} className='logo-who-we-are' alt='WiNGHacks Girls Who Code Logo'></img>
@@ -46,19 +61,22 @@ const AboutUs = () => {
         <div className='team-container'>
             <div className='team-child-container'>
               <div className = 'btn-committee-names'>
-              {/*MARIA-How to use committee name array so don't have to repeat button every time?*/}
-                <button>Main Organizers</button>
-                <button>Website</button>
-                <button>Merch</button>
+                {committeeNames.map ( (committeeName, index ) => {
+                  const filter = "group" + committeeName
+                  return <button id = {index}><a data-filter = {filter}>{committeeName}</a></button>
+              })}
               </div>
-
             </div>
             <div className='team-child-container'>
-              <div className = 'committeeMembers-container'>
-                <img  src={headshots} className='headshot' alt='WiNGHacks member photo'></img>
-                <img  src={headshots2} className='headshot' alt='WiNGHacks member photo'></img>
-              </div>
-
+              <ul className='members'>
+                {MemberList.map((member, index) => {
+                  const filter = "group" + member.committee_name
+                  return <CommitteeMemberItem id= {index} className = {filter} name={member.name} 
+                  major={member.major}  image={member.profile_pic} position={member.committee_position}
+                  linkedIn = {member.linkedIn}/>
+                })}
+              </ul>
+              
             </div>
         </div>
       </div>
---
+++ front-end/src/data/MemberList.js
@@ -0,0 +1,54 @@
+
+import littleLady_pic from '../components/pictures/littleLady.PNG'
+import frogger_pic from '../components/pictures/Frogger.PNG'
+import flier_pic from '../components/pictures/Flier.PNG'
+
+
+
+export const MemberList = [
+    {
+        id: "1",
+        name: "Little Lady (she/her)",
+        profile_pic: littleLady_pic,
+        linkedIn: "https://www.linkedin.com/company/uf-winghacks/",
+        major: "Computer Science",
+        committee_position: "Co-organizer",
+        committee_name: "Organizers",
+    },
+    {
+        id: "2",
+        name: "Frogger",
+        profile_pic: frogger_pic,
+        linkedIn: "https://www.linkedin.com/company/uf-winghacks/",
+        major: "Computer Science",
+        committee_position: "Co-organizer",
+        committee_name: "Organizers",
+    },
+    {
+        id: "3",
+        name: "Flier",
+        profile_pic: flier_pic,
+        linkedIn: "https://www.linkedin.com/company/uf-winghacks/",
+        major: "Computer Science",
+        committee_position: "Committee Head",
+        committee_name: "Awards",
+    },
+    {
+        id: "4",
+        name: "Flier 2",
+        profile_pic: flier_pic,
+        linkedIn: "https://www.linkedin.com/company/uf-winghacks/",
+        major: "Computer Science",
+        committee_position: "Committee Head",
+        committee_name: "Awards",
+    },
+    {
+        id: "5",
+        name: "Flier3",
+        profile_pic: flier_pic,
+        linkedIn: "https://www.linkedin.com/company/uf-winghacks/",
+        major: "Computer Science",
+        committee_position: "Committee Head",
+        committee_name: "Awards"
+    }
+]
