e36718a6d65610cca3c405aa2e146482ed498868
christinelin <christinelin.7771@gmail.com>
2024-02-09 00:56:21 -0500

    Added meet the team section

--- front-end/src/components/pages/AboutUs.js
+++ front-end/src/components/pages/AboutUs.js
@@ -1,4 +1,4 @@
-import React, {forwardRef} from 'react'
+import React, {forwardRef, useState} from 'react'
 import logos from '../pictures/WiNGHACKS_Logo_sansWings.png'
 
 import wicse from '../pictures/org_logos/wicse_logo.png'
@@ -7,8 +7,8 @@ import gwc from '../pictures/org_logos/gwc_logo.jpeg'
 import wece from '../pictures/org_logos/wece_logo.jpeg'
 import wicys from '../pictures/org_logos/wicys_logo.jpeg'
 
-import headshots from '../pictures/characters/littleLady.PNG'
-import headshots2 from '../pictures/characters/Frogger.PNG'
+import CommitteeMemberItem from './CommitteeMemberItem.js'
+import { MemberList } from '../data/MemberList.js'
 
 const AboutUs = ({}, ref) => {
 
@@ -17,7 +17,12 @@ const AboutUs = ({}, ref) => {
   The goal of our hackathon is to be a welcoming opportunity for learning and growth for underrepresented genders in the tech industry. Hackathons are great opportunities for students to learn new skills, network, and get a project on their resume, and we want to create an inclusive and welcoming environment for women and nonbinary students, especially those who are first-time hackers, to reap the benefits and opportunities that hackathons have. 
 
 `;
-  const committeeNames =["Main Organizers", "Awards", "Budget", "PR", "Merch", "Operations", "Sponsorship", "Tech-Support", "Website", "Workshop"];
+const committeeNames =["Directors", "Awards", "PR", "Merch", "Operations", "Sponsorship", "Tech-support", "Website", "Workshop"]; 
+  
+const [filterCommittee,setFilterCommittee] = useState("")  
+const updateFilterCommittee = (committeeName) => {
+  setFilterCommittee(committeeName)
+}
 
   return (
     <div ref={el => ref.current = { ...ref.current, about: el }}>
@@ -35,7 +40,7 @@ const AboutUs = ({}, ref) => {
             <div className='collaborators-container'>
               {/*MARIA-How to use variable to store the logos so don't have to repeat img every time?*/}
                 <a href="https://ufwicse.com" target="_blank"><img  src={wicse} className='logo-who-we-are' alt='WiNGHacks WiCSE Logo'></img></a>
-                <img  src={csk} className='logo-who-we-are' alt='WiNGHacks CSK Logo'></img>
+                <a href="https://www.linkedin.com/company/cs-kickstart-at-uf" target="_blank"><img  src={csk} className='logo-who-we-are' alt='WiNGHacks CSK Logo'></img></a>
                 <img  src={gwc} className='logo-who-we-are' alt='WiNGHacks Girls Who Code Logo'></img>
                 <img  src={wece} className='logo-who-we-are' alt='WiNGHacks Collaborator Logo'></img>
                 <img  src={wicys} className='logo-who-we-are' alt='WiNGHacks Collaborator Logo'></img>
@@ -44,23 +49,40 @@ const AboutUs = ({}, ref) => {
           
           </div>
         </div>
-        <h2>Meet the Team</h2>
+        <h2 style={{paddingTop: "8%"}}>Meet the Team</h2>
         <div className='team-container'>
-            <div className='team-child-container'>
-              <div className = 'btn-committee-names'>
-              {/*MARIA-How to use committee name array so don't have to repeat button every time?*/}
-                <button>Main Organizers</button>
-                <button>Website</button>
-                <button>Merch</button>
+            <div className='team-child-container-names'>
+              <div className='btn-committee-names'>
+                {committeeNames.map((committeeName, index) =>{
+                  return <button onClick={() =>updateFilterCommittee(committeeName)}>{committeeName}</button>
+                }
+                )
+                }
               </div>
 
             </div>
             <div className='team-child-container'>
-              <div className = 'committeeMembers-container'>
-                <img  src={headshots} className='headshot' alt='WiNGHacks member photo'></img>
-                <img  src={headshots2} className='headshot' alt='WiNGHacks member photo'></img>
-              </div>
-
+              <ul className='members'>
+                {MemberList.map((member, index) => {
+                  if (filterCommittee === ""){
+                    return   ( <div>
+                      <CommitteeMemberItem  name={member.name} 
+                      image={member.profile_pic} position={member.committee_position}
+                     linkedIn = {member.linkedIn}/> 
+                     </div>)
+                  }
+                  if (member.committee_name === filterCommittee) {
+                     return   ( <div>
+                     <CommitteeMemberItem  name={member.name} 
+                     image={member.profile_pic} position={member.committee_position}
+                    linkedIn = {member.linkedIn}/> 
+                    </div>)
+                 
+                  }
+                  
+                })}
+              </ul>
+              
             </div>
         </div>
       </div>
