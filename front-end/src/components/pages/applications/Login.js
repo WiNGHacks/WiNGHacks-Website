fbf92627699c25be5130feb9b61ece1f56e6a36d
AnanyaKakaveti <a.kakaveti@ufl.edu>
2024-03-02 16:51:07 -0500

    mlh badge added

--- front-end/src/components/pages/applications/Login.js
+++ front-end/src/components/pages/applications/Login.js
@@ -134,7 +134,7 @@ const Login = () => {
                     />
                     {/* {console.log(password)} */}
                     <p style={{marginTop : "-1rem", fontSize: "1rem"}}>
-                        <a href="/forgetPassword" className='forget-password-link'>Forget password?</a>
+                        <a href="/forgetPassword" className='forget-password-link'>Forgot password?</a>
                     </p>
 
                     {submitedClicked?(
