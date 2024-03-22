6e011ff0dedf46e6d7e1c156dc0a81f062c33e47
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2024-03-22 18:32:03 -0400

    Fixed header email titles

--- back-end/routes/Users.js
+++ back-end/routes/Users.js
@@ -58,7 +58,7 @@ const emailWelcomeTemplate = {
 `}
 
 const emailRejectedTemplate = (name) => {
-    return{subject: "Thank for your application",
+    return{subject: "Thank you for your application",
     content:`
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
