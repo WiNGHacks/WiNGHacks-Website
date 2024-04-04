04b1c8b7a77b597d6a7295a163f4bae1c77e9c9f
mariapasaylo <mnapasaylo@gmail.com>
2024-04-04 00:32:03 -0400

    updated information of awards

--- front-end/src/components/data/AwardList.js
+++ front-end/src/components/data/AwardList.js
@@ -5,7 +5,7 @@ import Best_WiNG from '../pictures/characters/gifs/frogger.gif';
 import Best_Game from '../pictures/characters/gifs/mushwoom.gif';
 import Best_XR from '../pictures/characters/gifs/draggin.gif';
 import Sponsor_Placeholder from '../pictures/characters/gifs/seaButterfly.gif';
-
+import infotech_award from '../pictures/sponsor_logo/infotech-logo.png'
 
 export const AwardList = [
     {
@@ -15,13 +15,13 @@ export const AwardList = [
         description:"Awarded to unparalleled excellence in a project's innovation and execution.",
         alt: 'Best Overall Award', 
         tag: 'General',
-        sponsored:"Sponsored by JPMC",
+        sponsored:"Sponsored by JP Morgan Chase",
     },
     {
         id: "2",
         logo_image: First_Flight,
         title: "First Flight Award",
-        description:"Given to a team made up of only first-time hackers; shows exemplary performance in their first hackathon.",
+        description:"Best project made by a team of ONLY first-time hackers.",
         alt: 'First Flight Award', 
         tag: 'General',
         sponsored:"",
@@ -30,7 +30,7 @@ export const AwardList = [
         id: "3",
         logo_image: Peoples_Choice,
         title: "People's Choice Award",
-        description:"Voted on through DevPost during a set amount of time once the competition ends.",
+        description:"The project with the most community votes on DevPost",
         alt: "People's Choice Award", 
         tag: 'General',
         sponsored:"",
@@ -39,7 +39,7 @@ export const AwardList = [
         id: "4",
         logo_image: Best_WiNG,
         title: "Best WiNG Hack",
-        description:"Best tackles an issue related to women, gender-nonconforming and/or marginalized communities.",
+        description:"Best tackles an issue related to women, non-binary, gender-nonconforming communities.",
         alt: "Best WiNG Hack", 
         tag: 'Category',
         sponsored:"",
@@ -48,7 +48,7 @@ export const AwardList = [
         id: "5",
         logo_image: Best_Game,
         title: "Best Game Hack",
-        description:"Most innovative game designs, including trailblazing mechanics to boundary-breaking narratives.",
+        description:"Most innovative game designs, including  mechanics to narratives.",
         alt: "Best Game Hack", 
         tag: 'Category',
         sponsored:"",
@@ -57,13 +57,22 @@ export const AwardList = [
         id: "6",
         logo_image: Best_XR,
         title: "Best XR Hack",
-        description:"Recognizes exceptional use of extended reality as a medium. ",
+        description:"Recognizes exceptional use of extended reality as a medium.",
         alt: "Best XR Hack", 
         tag: 'Category',
-        sponsored:"Supported by Society of PC Building and GatorVR",
+        sponsored:"Presented by Society of PC Building and GatorVR",
     },
     {
         id: "7",
+        logo_image: infotech_award,
+        title: "Infotech Challenge",
+        description:"Most innovative use of a public dataset for the public good",
+        alt: "Infotech Challenge", 
+        tag: 'Sponsor',
+        sponsored:"",
+    },
+    {
+        id: "8",
         logo_image: Sponsor_Placeholder,
         title: "Sponsor Challenge",
         description:"More info soon!",
