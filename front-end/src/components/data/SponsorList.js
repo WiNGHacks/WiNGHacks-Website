67b63943d72218ff1c808ebaff580e3f3ff830d3
kellyyuan333 <kellyyuan333@gmail.com>
2025-02-04 11:18:28 -0500

    update sponsors

--- front-end/src/components/data/SponsorList.js
+++ front-end/src/components/data/SponsorList.js
@@ -1,118 +1,83 @@
-import bloomberg_logo from '../pictures/sponsor_logo/bloomberg-logo.png'
-// import rtc_logo from '../pictures/sponsor_logo/rtc-logo.png'
-import c1_logo from '../pictures/sponsor_logo/capitalone-logo.png'
-// import TI_logo from '../pictures/sponsor_logo/TI-logo.png'
+import Bloomberg_logo from '../pictures/sponsor_logo/bloomberg-logo.png'
+import C1_logo from '../pictures/sponsor_logo/capitalone-logo.png'
 import JPMC_logo from '../pictures/sponsor_logo/jpmc-logo.png'
-// import UKG_logo from '../pictures/sponsor_logo/ukg-logo.png'
 import Infotech_logo from '../pictures/sponsor_logo/infotech-logo.png'
-// import GitHub_logo from '../pictures/sponsor_logo/github-logo.png'
 import StandoutSticker_logo from '../pictures/sponsor_logo/standoutsticker-logo.png'
-// import EA_logo from '../pictures/sponsor_logo/ea-logo.png'
 import BNYMellon_logo from '../pictures/sponsor_logo/bnymellon-logo.png'
-import VSPVision_logo from '../pictures/sponsor_logo/vspvision-logo.png'
 import MLH_logo from '../pictures/sponsor_logo/mlh-logo.png'
 import Epsilon_logo from '../pictures/sponsor_logo/epsilon-logo.svg'
 import NordVPN_logo from '../pictures/sponsor_logo/nordvpn-logo.png'
+import Dominos_logo from '../pictures/sponsor_logo/dominos-logo.png'
 
 export const SponsorList = [
     {
         id: "1",
-        logo_image: bloomberg_logo,
+        logo_image: Bloomberg_logo,
         url: "https://www.bloomberg.com/",
         alt: 'Bloomberg Logo', 
         tier: 'Silver',
     },
-    // {
-    //     id: "2",
-    //     logo_image: rtc_logo,
-    //     url: "https://rewritingthecode.org/",
-    //     alt: 'RTC Logo'
-    // },
     {
-        id: "3",
-        logo_image: c1_logo,
+        id: "2",
+        logo_image: C1_logo,
         url: "https://www.capitalone.com/",
         alt: 'Capital One Logo',
         tier: 'Silver',
     },
-    // {
-    //     id: "4",
-    //     logo_image: TI_logo,
-    //     url: "https://www.ti.com/",
-    //     alt: 'Texas Instrument Logo'
-    // },
     {
-        id: "5",
+        id: "3",
         logo_image: JPMC_logo,
         url: "https://www.jpmorganchase.com/",
         alt: 'JPMC Logo',
         tier: 'Co-Host'
     },
-    // {
-    //     id: "6",
-    //     logo_image: UKG_logo,
-    //     url: "https://www.ukg.com/",
-    //     alt: 'UKG Logo'
-    // },
     {
-        id: "7",
+        id: "4",
         logo_image: Infotech_logo,
         url: "https://www.infotechinc.com/",
         alt: 'Infotech Logo',
         tier: 'Silver',
     },
-    // {
-    //     id: "8",
-    //     logo_image: GitHub_logo,
-    //     url: "https://github.com",
-    //     alt: 'GitHub Logo'
-    // },
     {
-        id: "9",
+        id: "5",
         logo_image: StandoutSticker_logo,
         url: "https://www.hackp.ac/mlh-standoutstickers-hackathons/",
-        alt: 'StandoutSticker_logo',
+        alt: 'StandoutSticker Logo',
         tier: 'Other',
     },
-    // {
-    //     id: "10",
-    //     logo_image: EA_logo,
-    //     url: "https://www.ea.com/",
-    //     alt: 'EA_logo'
-    // },
     {
-        id: "11",
+        id: "6",
         logo_image: BNYMellon_logo,
         url: "https://www.bnymellon.com/",
-        alt: 'BNYMellon_logo',
+        alt: 'BNYMellon Logo',
         tier: 'Platinum',
     },
     {
-        id: "12",
-        logo_image: VSPVision_logo,
-        url: "https://www.vsp.com/",
-        alt: 'VSPVision_logo',
-        tier: 'Gold',
-    },
-    {
-        id: "13",
+        id: "7",
         logo_image: MLH_logo,
         url: "https://mlh.io/",
-        alt: 'MLH_logo',
+        alt: 'MLH Logo',
         tier: 'Other',
     },
     {
-        id: "14",
+        id: "8",
         logo_image: Epsilon_logo,
         url: "https://www.epsilonsystems.com/",
-        alt: 'Epislon_logo',
+        alt: 'Epislon Logo',
         tier: 'Bronze',
     },
     {
-        id: "15",
+        id: "9",
         logo_image: NordVPN_logo,
         url: "https://nordvpn.com/nord-site/",
-        alt: 'NordVPN_logo',
+        alt: 'NordVPN Logo',
+        tier: 'Other',
+    },
+    {
+        id: "10",
+        logo_image: Dominos_logo,
+        url: "https://www.dominos.com/en/",
+        alt: 'Dominos Logo',
         tier: 'Other',
     },
 
