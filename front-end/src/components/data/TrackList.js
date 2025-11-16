13b4f3190ec5406445b54eb37f03242f8cad56a7
Ivette <ivette.saldanahernandez@gmail.com>
2025-11-16 14:37:31 -0500

    feat: added carousel to track section

---
+++ front-end/src/components/data/TrackList.js
@@ -0,0 +1,36 @@
+// import images for icons
+import GaytorIcon from '../pictures/characters/Gay-tor.PNG';
+
+export const TrackList = [
+    // add future tracks here
+  {
+    id: 1,
+    title: 'Track 1',
+    description: 'This is the description for Track 1.',
+    icon: GaytorIcon, // example
+  },
+  {
+    id: 2,
+    title: '',
+    description: '',
+    icon: '',
+  },
+  {
+    id: 3,
+    title: '',
+    description: '',
+    icon: '',
+  },
+  {
+    id: 4,
+    title: '',
+    description: '',
+    icon: '',
+  },
+  {
+    id: 5,
+    title: '',
+    description: '',
+    icon: '',
+  },
+];
--- front-end/src/components/pages/Track.js
+++ front-end/src/components/pages/Track.js
@@ -1,11 +1,31 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { TrackList } from '../data/TrackList';
 
 const Track = ({}, ref) => {
+  const responsive = {
+    superLargeDesktop: { breakpoint: { max: 4000, min: 1300 }, items: 4, slidesToSlide: 2 },
+    desktop:           { breakpoint: { max: 1300, min: 1000 }, items: 3 },
+    tablet:            { breakpoint: { max: 1000, min: 750 },  items: 2 },
+    mobile:            { breakpoint: { max: 750,  min: 0 },   items: 1 },
+  };
+  
   return (
     <div ref={el => ref.current = { ...ref.current, track: el }}>
-      <div className='Page'>
-        <h1>Track</h1>
-        <p>To be updated.</p>
+      <div className='Page Track'>
+        <h1>Tracks</h1>
+        <Carousel responsive={responsive} showDots={true}>
+          {TrackList.map(track => (
+            <div className="card" key={track.id}>
+              {track.icon && <img className="product-image" src={track.icon} alt={track.title} />}
+              <div className="content">
+                <h3 className="title">{track.title}</h3>
+                <p className="description">{track.description}</p>
+              </div>
+            </div>
+          ))}
+        </Carousel>
       </div>
     </div>
   )
