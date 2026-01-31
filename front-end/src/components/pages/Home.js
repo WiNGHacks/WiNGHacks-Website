726d3b99ba2f87b335b77c6abd6cf3a1f6b446cd
Ivette Saldana Hernandez <166746949+IvetteSaldanaHernandez@users.noreply.github.com>
2026-01-30 20:53:33 -0500

    added dates and location
    am gonna add the socials to nav bar later

--- front-end/src/components/pages/Home.js
+++ front-end/src/components/pages/Home.js
@@ -22,18 +22,31 @@ const Home = ({}, ref) => {
   return (
     // ref = {el => ref.current = { ...ref.current, home: el }}
     <div className='Home'>
+      <div className="home-details">
+        {/* <h1>WiNGHacks 2026</h1> */}
+        {/* <h2>February 21–23, 2026</h2> */}
+        <span className="home-detail">
+          📅 Feb 20–22, 2026
+        </span>
+        <span className="home-detail">
+          📍 Newell Hall — Gainesville, FL
+        </span>
+      </div>
+      {/* Applications closed, can uncomment when opened again */}
       <div className='home-buttons' align="center">
         {/* last year's become a mentor form: https://docs.google.com/forms/d/e/1FAIpQLSeTqVfJUiWjjARDFBh1MkegBLw0F-Kd-GquU2NJO1-H7ybOuQ/viewform */}
-        <a href="https://docs.google.com/forms/d/1_T1ST79ov7YmytljRhkaZLRcnQ_d0dinQzUuHem_Rps/viewform?edit_requested=true"
+        {/* <a href="https://docs.google.com/forms/d/1_T1ST79ov7YmytljRhkaZLRcnQ_d0dinQzUuHem_Rps/viewform?edit_requested=true"
               target="_blank">
               <button className='Button Button-home Button-interest'>Become a Mentor</button></a>
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdusvJ_vntBDq33Dqx5DgR0LXx7So5OtFqxHxqETg2j1qWn0Q/viewform"
               target="_blank">
-              <button className='Button Button-home Button-hacker'>Become a Hacker!</button></a>
+              <button className='Button Button-home Button-hacker'>Become a Hacker!</button></a> */}
         {/* <a href={process.env.REACT_APP_APPLICATION_FORM} target="_blank"><button className='Button Button-home Button-apply button-glow-home-apply'>Link to Apply!</button></a> */}
-        <a><button onClick={downloadPDF} className='Button Button-home Button-sponsor'>Sponsorship Packet</button></a>
+        {/* <a><button onClick={downloadPDF} className='Button Button-home Button-sponsor'>Sponsorship Packet</button></a> */}
       </div>
-      <div className='socials'>
+
+      {/* moved socials to top/nav bar */}
+      {/* <div className='socials'>
           <a href= "https://www.instagram.com/winghacks/" target="_blank">
             <FaSquareInstagram className='icons'/>
           </a>
@@ -43,7 +56,7 @@ const Home = ({}, ref) => {
           <a href="mailto: uf.winghacks@gmail.com" target="_blank">
             <IoMdMail className='icons'/>
           </a>
-      </div>
+      </div> */}
   </div>
   )
 }
