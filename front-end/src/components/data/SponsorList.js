5b28cf2a7fb6bca4c239af1a597c8c18ca2d30e2
Ivette Saldana Hernandez <166746949+IvetteSaldanaHernandez@users.noreply.github.com>
2026-02-11 17:15:57 -0500

    updated sponsors
    reflects current sponsors

--- front-end/src/components/data/SponsorList.js
+++ front-end/src/components/data/SponsorList.js
@@ -1,123 +1,107 @@
-import Bloomberg_logo from '../pictures/sponsor_logo/bloomberg-logo.png'
-import C1_logo from '../pictures/sponsor_logo/capitalone-logo.png'
-import JPMC_logo from '../pictures/sponsor_logo/jpmc-logo.png'
-import Infotech_logo from '../pictures/sponsor_logo/infotech-logo.png'
-import StandoutSticker_logo from '../pictures/sponsor_logo/standoutsticker-logo.png'
 import BNYMellon_logo from '../pictures/sponsor_logo/bnymellon-logo.png'
-import MLH_logo from '../pictures/sponsor_logo/mlh-logo.png'
+import Twocents_logo from '../pictures/sponsor_logo/twocents-logo.png'
+import DoEL_logo from '../pictures/sponsor_logo/DoEL.png'
+import ECE_logo from '../pictures/sponsor_logo/ece-logo.png'
 import GitHub_logo from '../pictures/sponsor_logo/github-logo.png'
 import Buttons_logo from '../pictures/sponsor_logo/purebuttons-logo.png'
 import Epsilon_logo from '../pictures/sponsor_logo/epsilon-logo.svg'
-import Dominos_logo from '../pictures/sponsor_logo/dominos-logo.png'
-import NordVPN_logo from '../pictures/sponsor_logo/nordvpn-logo.png'
-import NordPass_logo from '../pictures/sponsor_logo/nordpass-logo.png'
-import Cog_logo from '../pictures/sponsor_logo/cog-logo.png'
-import Saily_logo from '../pictures/sponsor_logo/saily-logo.png'
+import TI_logo from '../pictures/sponsor_logo/TI-logo.png'
+import IEEE_logo from '../pictures/sponsor_logo/IEEE-logo.jpeg'
+import Celsius_logo from '../pictures/sponsor_logo/Celsius-Logo.png'
+import Monster_Energy_logo from '../pictures/sponsor_logo/Monster_Energy_logo.png'
+import Snag_logo from '../pictures/sponsor_logo/snag-logo.avif'
+import DSI_logo from '../pictures/sponsor_logo/dsi-logo.jpg'
 
 export const SponsorList = [
-    // {
-    //     id: "1",
-    //     logo_image: Bloomberg_logo,
-    //     url: "https://www.bloomberg.com/",
-    //     alt: 'Bloomberg Logo', 
-    //     tier: 'Silver',
-    // },
-    // {
-    //     id: "2",
-    //     logo_image: C1_logo,
-    //     url: "https://www.capitalone.com/",
-    //     alt: 'Capital One Logo',
-    //     tier: 'Silver',
-    // },
-    // {
-    //     id: "3",
-    //     logo_image: JPMC_logo,
-    //     url: "https://www.jpmorganchase.com/",
-    //     alt: 'JPMC Logo',
-    //     tier: 'Co-Host'
-    // },
-    // {
-    //     id: "4",
-    //     logo_image: Infotech_logo,
-    //     url: "https://www.infotechinc.com/",
-    //     alt: 'Infotech Logo',
-    //     tier: 'Silver',
-    // },
-    // {
-    //     id: "5",
-    //     logo_image: StandoutSticker_logo,
-    //     url: "https://www.hackp.ac/mlh-standoutstickers-hackathons/",
-    //     alt: 'StandoutSticker Logo',
-    //     tier: 'Other',
-    // },
-    // {
-    //     id: "6",
-    //     logo_image: BNYMellon_logo,
-    //     url: "https://www.bnymellon.com/",
-    //     alt: 'BNYMellon Logo',
-    //     tier: 'Platinum',
-    // },
-    // {
-    //     id: "7",
-    //     logo_image: MLH_logo,
-    //     url: "https://mlh.io/",
-    //     alt: 'MLH Logo',
-    //     tier: 'Other',
-    // },
-    // {
-    //     id: "8",
-    //     logo_image: Epsilon_logo,
-    //     url: "https://www.epsilonsystems.com/",
-    //     alt: 'Epislon Logo',
-    //     tier: 'Bronze',
-    // },
-    // {
-    //     id: "9",
-    //     logo_image: Dominos_logo,
-    //     url: "https://www.dominos.com/en/",
-    //     alt: 'Dominos Logo',
-    //     tier: 'Other',
-    // },
-    // {
-    //     id: "10",
-    //     logo_image: NordVPN_logo,
-    //     url: "https://nordvpn.com/hackathons",
-    //     alt: 'NordVPN Logo',
-    //     tier: 'Other',
-    // },
-    // {
-    //     id: "11",
-    //     logo_image: Cog_logo,
-    //     url: "https://incogni.com/",
-    //     alt: 'Cog Logo',
-    //     tier: 'Other',
-    // },
-    // {
-    //     id: "12",
-    //     logo_image: NordPass_logo,
-    //     url: "https://nordpass.com/",
-    //     alt: 'NordPass Logo',
-    //     tier: 'Other',
-    // },
-    // {
-    //     id: "13",
-    //     logo_image: Saily_logo,
-    //     url: "https://saily.com/",
-    //     alt: 'Saily Logo',
-    //     tier: 'Other',
-    // },
     {
-        id: "14",
+        id: "1",
+        logo_image: BNYMellon_logo,
+        url: "https://www.bnymellon.com/",
+        alt: 'BNYMellon Logo',
+        tier: 'Platinum',
+    }, 
+    {
+        id: "2",
+        logo_image: Twocents_logo,
+        url: "https://www.twocents.io/",
+        alt: 'Twocents Logo',
+        tier: 'Platinum',
+    },
+    {
+        id: "3",
+        logo_image: DoEL_logo,
+        url: "https://www.ece.ufl.edu/academics/undergraduate/experiential-learning/", //update
+        alt: 'Department of Experiential Learning Logo', 
+        tier: 'Gold',
+    },
+    {
+        id: "4",
+        logo_image: ECE_logo, 
+        url: "https://www.ece.ufl.edu/", //update
+        alt: 'ECE Department Logo',
+        tier: 'Silver',
+    },
+    {
+        id: "5",
+        logo_image: Epsilon_logo,
+        url: "https://www.epsilonsystems.com/",
+        alt: 'Epislon Logo',
+        tier: 'Bronze',
+    },
+    {
+        id: "6",
+        logo_image: TI_logo,
+        url: "https://www.ti.com/",
+        alt: 'TI Logo',
+        tier: 'Bronze',
+    },
+    {
+        id: "7",
+        logo_image: IEEE_logo,
+        url: "https://ieee.ece.ufl.edu/",
+        alt: 'IEE Logo',
+        tier: 'Bronze',
+    },
+    {
+        id: "8",
         logo_image: GitHub_logo,
         url: "https://github.com/",
         alt: 'GitHub Logo',
         tier: 'Other',
     },
     {
-        id: "15",
+        id: "9",
         logo_image: Buttons_logo,
         url: "https://www.purebuttons.com/?srsltid=AfmBOorfJTNFsM6iwlqLCYyeHfADYpRP8T4l8oksIMxfyCuD7vj-dSNq",
         alt: 'Pure Buttons Logo',
+        tier: 'Other',
+    },
+    {
+        id: "10",
+        logo_image: Celsius_logo,
+        url: "https://www.celsius.com/", 
+        alt: 'Celsius Logo',
+        tier: 'Other',
+    },
+    {
+        id: "11",
+        logo_image: Monster_Energy_logo,
+        url: "https://www.monsterenergy.com/en-us/", 
+        alt: 'Monster Logo',
+        tier: 'Other',
+    },
+    {
+        id: "12",
+        logo_image: Snag_logo,
+        url: "https://snagdelivery.app/",
+        alt: 'Snag Logo',
+        tier: 'Other',
+    },
+    {
+        id: "13",
+        logo_image: DSI_logo,
+        url: "https://www.ufdsi.com/",
+        alt: 'DSI Logo',
         tier: 'Other',
     },
 ]
